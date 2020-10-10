46cf5f3727d8884a6d7d39f20358276089cfea01
ikawaha <ikawaha@users.noreply.github.com>
2020-10-10 21:32:56 +0900

    Bump kagome-dict v1.0.1 and rebuild Uni dict

--- uni/internal/data/bindata06.go
+++ uni/internal/data/bindata06.go
@@ -17,7 +17,7 @@ func dictUnidictAg() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "dict/unidict.ag", size: 524288, mode: os.FileMode(420), modTime: time.Unix(1600786977, 0)}
+	info := bindataFileInfo{name: "dict/unidict.ag", size: 524288, mode: os.FileMode(420), modTime: time.Unix(1602332898, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
--- uni/tool/go.mod
+++ uni/tool/go.mod
@@ -1,8 +1,8 @@
 module tool
 
-go 1.14
+go 1.15
 
 require (
-	github.com/ikawaha/kagome-dict v0.0.1
+	github.com/ikawaha/kagome-dict v1.0.1
 	golang.org/x/text v0.3.3
 )
